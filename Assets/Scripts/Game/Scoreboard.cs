b3a96e032aa415644c139b626187cf40903abcb2
Keijiro Takahashi <keijiro@gmail.com>
2026-01-06 14:34:53 +0900

    Scoreboard animation

--- Assets/Scripts/Game/Scoreboard.cs
+++ Assets/Scripts/Game/Scoreboard.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public sealed class Scoreboard : MonoBehaviour
 {
@@ -7,6 +8,16 @@ public sealed class Scoreboard : MonoBehaviour
     [Space]
     [SerializeField] ParticleSystem _coinEmitter = null;
     [SerializeField] ParticleSystem _heartEmitter = null;
+    [SerializeField] int _scoreSpeed = 500;
+
+    #endregion
+
+    #region Private Members
+
+    UIDocument _ui;
+    Label _scoreText;
+
+    (int current, int display) _score;
 
     #endregion
 
@@ -16,11 +27,49 @@ public sealed class Scoreboard : MonoBehaviour
     {
         _coinEmitter.Emit(20);
         _heartEmitter.Play();
+        _score.current += 20;
     }
 
     public void Tip()
     {
         _coinEmitter.Emit(1);
+        _score.current++;
+    }
+
+    public void Penalize()
+    {
+        _score.current -= 10;
+    }
+
+    #endregion
+
+    #region MonoBehaviour Methods
+
+    void Start()
+    {
+        _ui = GetComponent<UIDocument>();
+        _scoreText = _ui.rootVisualElement.Q<Label>("score-text");
+        _score.current = 1000;
+    }
+
+    void Update()
+    {
+        if (_score.display < _score.current)
+        {
+            _score.display += (int)(_scoreSpeed * Time.deltaTime);
+            _score.display = Mathf.Min(_score.display, _score.current);
+        }
+        else if (_score.display > _score.current)
+        {
+            _score.display -= (int)(_scoreSpeed * Time.deltaTime);
+            _score.display = Mathf.Max(_score.display, _score.current);
+        }
+        else
+        {
+            return;
+        }
+
+        _scoreText.text = $"{_score.display:N0}";
     }
 
     #endregion
